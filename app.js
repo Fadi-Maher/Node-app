49852e80014154b7551b0948e3ad77421eb4530f
Fadi-Maher <cleverfady@gmail.com>
2025-02-25 19:00:12 +0200

    send data from mongo to browser is done

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
+const myUser = require("./models/myDataSchema");
+app.set("view engine", "ejs");
 
- const myScema = require("./models/myDataSchema");
+app.get("/", (req, res) => {
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+myUser
+  .findOne()
+  .then((result) => {
+    console.log(result);
+    res.render("home", { mytitle: "test home page" , myUsers: result});
+
+  })
+  .catch((err) => {
+    err.message;
+  });
 });
-
-app.get('/feedBack.html', (req, res) => {
+ 
+app.get("/feedBack.html", (req, res) => {
   res.send("<h1>User name is sent</h1>");
 });
 
-mongoose.connect('mongodb+srv://Fady:Fady123@cluster0.2dhey.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+mongoose
+  .connect(
+    "mongodb+srv://Fady:Fady123@cluster0.2dhey.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
@@ -24,53 +37,11 @@ mongoose.connect('mongodb+srv://Fady:Fady123@cluster0.2dhey.mongodb.net/all-data
     console.log("Error connecting to MongoDB:", err);
   });
 
- app.post('/', async (req, res) => {
-   
-    console.log(req.body);   
+app.post("/", async (req, res) => {
+  console.log(req.body);
 
-  
-    const article = new myScema(req.body);  
-    await article.save(); 
+  const article = new myUser(req.body);
+  await article.save();
 
-    res.redirect("/feedBack.html");   
- 
+  res.redirect("/feedBack.html");
 });
-
- 
-  
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// const mongoose = require('mongoose');
-// app.use(express.urlencoded({ extended: true }));
-
-// // Import the model (only once from the schema file)
-// const Mydata = require("./models/myDataSchema");
-
-// app.get('/', (req, res) => {
-//   res.sendFile("./views/home.html", { root: __dirname });
-// });
-
-// mongoose.connect('mongodb+srv://Fady:Fady123@cluster0.2dhey.mongodb.net/all-data?retryWrites=true&w=majority')
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Example app listening on port ${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("Error connecting to MongoDB:", err);
-//   });
-
-// app.post("/", async (req, res) => {
-//   try {
-//     console.log(req.body);  // Log the request body to the console
-
-//     const article = new Mydata(req.body);  // Create a new article using the data in req.body
-//     await article.save();  // Save the article to the database
-//     res.redirect("/");  // Redirect to the home page after saving
-//   } catch (err) {
-//     console.error("Error saving article:", err);
-//     res.status(500).send("Error saving article");
-//   }
-// });
--- models/myDataSchema.js
+++ models/myDataSchema.js
@@ -19,19 +19,4 @@ const Schema = mongoose.Schema;
 
 
 
-
-
-// // models/myDataSchema.js
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// // Define schema 
-// const articleSchema = new Schema({
-//     userName: String
-// });
-
-// // Compile schema into model (This will automatically use "mydatas" as the collection name)
-// const Mydata = mongoose.model("Mydata", articleSchema);  // The collection name will be "mydatas"
-
-// // Export model
-// module.exports = Mydata;
+ 
